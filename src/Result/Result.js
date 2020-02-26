72a1df559993a8dd081aa6ce20ced8557a400591
Christopher <christopher@seekingwebdev.com>
2020-02-26 14:09:13 +0000

    pass down savings and annual spend

--- src/Result/Result.js
+++ src/Result/Result.js
@@ -4,9 +4,9 @@ export default function Result(props) {
   return (
     <div>
       <h3>Supplier: {props.supplierName}</h3>
-      <p>Expected Annual Savings: </p>
-      <p>Expected Annual Spend: </p>
-      <p>Expected Monthly Spend: </p>
+      <p>Expected Annual Savings: £{props.expectedAnnualSavings}</p>
+      <p>Expected Annual Spend: £{props.expectedAnnualSpend}</p>
+      {/* <p>Expected Monthly Spend: </p> */}
     </div>
   )
 }
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,31 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Result from '../Result/Result';
 
 export default function Results() {
-  const [data, setData] = useState({elecResults: []})
+  const [data, setData] = useState({ elecResults: [] });
   const getData = () => {
-    const apiKey = 'a8823b4b-1abe-41de-a5b3-ab6700c08d98'
-    const url = `https://demo.staging.energyhelpline-aws.com/api/results/${apiKey}`
+    const apiKey = 'a8823b4b-1abe-41de-a5b3-ab6700c08d98';
+    const url = `https://demo.staging.energyhelpline-aws.com/api/results/${apiKey}`;
     fetch(url)
       .then(res => res.json())
       .then(json => {
-        console.log(json.elecResults)
-        setData({elecResults: json.elecResults})
-      })
-  }
-  
+        console.log(json.elecResults);
+        setData({ elecResults: json.elecResults });
+      });
+  };
+
   useEffect(() => {
-    getData()
+    getData();
   }, []);
-  
+
   return (
     <div>
-      <h2>
-        Results
-      </h2>
+      <h2>Results</h2>
       {data.elecResults.map((result, index) => (
-        <Result key={index} supplierName={result.supplierName}/>
+        <Result
+        key={index}
+        supplierName={result.supplierName}
+        expectedAnnualSavings={result.expectedAnnualSavings}
+        expectedAnnualSpend={result.expectedAnnualSpend}
+        />
       ))}
     </div>
-  )
+  );
 }
