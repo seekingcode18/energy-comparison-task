0404d86a9bd2a03afa26561f7a07f363d46e7887
Christopher <christopher@seekingwebdev.com>
2020-02-26 14:31:06 +0000

    test that the title renders

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Results from '../Results/Results';
 
-test('renders learn react link', () => {
+test('renders the app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = getByText('Energy Comparison');
+  expect(title).toBeInTheDocument();
 });
+
